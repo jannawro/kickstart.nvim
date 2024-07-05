2513d4b61c73d83511358d5b0196cbd4f093281a
Jan Nawrocki <jnawrocki@splunk.com>
2024-07-05 20:13:41 +0200

    "

--- init.lua
+++ init.lua
@@ -650,6 +650,7 @@ require('lazy').setup({
               autoImportCompletion = true,
             },
             python = {
+              pythonPath = '.venv/bin/python',
               analysis = {
                 autoSearchPaths = true,
                 diagnosticMode = 'openFilesOnly',
@@ -782,12 +783,12 @@ require('lazy').setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
       },
       'saadparwaiz1/cmp_luasnip',
