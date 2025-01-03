0dbad5e25aaf5c41cbc72fced60354848dcdeeea
Jan Nawrocki <jan.nawrocki06@gmail.com>
2025-01-03 16:14:53 +0100

    update

--- init.lua
+++ init.lua
@@ -362,6 +362,7 @@ require('lazy').setup({
       spec = {
         { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
         { '<leader>d', group = '[D]iagnostic' },
+        { '<leader>f', group = '[F]ile' },
         { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch' },
         { '<leader>t', group = '[T]oggle' },
--- lua/custom/commands/telescope_file_browser.lua
+++ lua/custom/commands/telescope_file_browser.lua
@@ -1,3 +1,3 @@
-vim.keymap.set('n', '<leader>fb', ':Telescope file_browser path=%:p:h select_buffer=true<CR>')
+vim.keymap.set('n', '<leader>fb', ':Telescope file_browser path=%:p:h select_buffer=true<CR>', { desc = 'Open [F]ile [B]rowser' })
 
 return {}
